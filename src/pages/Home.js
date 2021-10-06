999cb1cebc89235de80a59ce55daf56242a9caa0
blockchainer2022 <=blockchainer.2202@gmail.com>
2021-10-06 11:42:03 +0530

    update logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
-import React from "react";
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from "react";
 import { Layout } from "../components";
+import Uparrow from "../assets/images/up.png";
 import {
   HeroSection,
   AboutSection,
@@ -9,8 +11,25 @@ import {
   FaqSection,
 } from "../sections";
 const Home = () => {
+  const [show, setShow] = useState(false);
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 500) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  });
+
   return (
     <div>
+      <a
+        href="#"
+        className={`" bg-primary text-white click-to-top rounded-full ${
+          show ? "show" : null
+        } "`}
+      >
+        <img src={Uparrow} alt="" className="w-5" />
+      </a>
       <Layout>
         <HeroSection />
         <AboutSection />
